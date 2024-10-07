d302a33a2471e129b0f14ce407c7c3c606063aa5
JosueVJ <josuevj.1e.49@gmail.com>
2024-10-06 19:51:41 -0600

    añadiendo marcador

--- juego-pong.py
+++ juego-pong.py
@@ -6,28 +6,60 @@ from pygame.locals import QUIT
 VENTANA_HORI = 1200  
 VENTANA_VERT = 600  
 FPS = 60  
+
 NEGRO = (0, 0, 0)  
 AZUL = (0, 0, 255) 
 RED = (255, 0, 0) 
 TAM_CIRCULO = 10  
 
+class Screen:
+    def __init__(self):
+        self.ventana = pygame.display.set_mode((VENTANA_HORI, VENTANA_VERT))
+        self.ventana.fill(NEGRO)
+        pygame.display.set_caption("Pong Game")
+        self.game_state = "start_menu"
+
+    def draw_start_menu(self):
+        self.ventana.fill(NEGRO)
+        font = pygame.font.SysFont('arial', 40)
+        title = font.render('Mayate`s game', True, (255, 255, 255))
+        start_button = font.render('Start', True, (255, 255, 255))
+        self.ventana.blit(title, (VENTANA_HORI/2 - title.get_width()/2, VENTANA_VERT/2 - title.get_height()/2))
+        self.ventana.blit(start_button, (VENTANA_HORI/2 - start_button.get_width()/2, VENTANA_VERT/2 + start_button.get_height()/2))
+        pygame.display.update()
+
+    def draw_game_over_screen(self):
+        self.ventana.fill(NEGRO)
+        font = pygame.font.SysFont('arial', 40)
+        title = font.render('Game Over', True, (255, 255, 255))
+        restart_button = font.render('R - Restart', True, (255, 255, 255))
+        quit_button = font.render('Q - Quit', True, (255, 255, 255))
+        self.ventana.blit(title, (VENTANA_HORI/2 - title.get_width()/2, VENTANA_VERT/2 - title.get_height()/3))
+        self.ventana.blit(restart_button, (VENTANA_HORI/2 - restart_button.get_width()/2, VENTANA_VERT/1.9 + restart_button.get_height()))
+        self.ventana.blit(quit_button, (VENTANA_HORI/2 - quit_button.get_width()/2, VENTANA_VERT/2 + quit_button.get_height()/2))
+        pygame.display.update()
+
+    def draw_score(self, puntaje_jugador_1, puntaje_jugador_2):
+        font = pygame.font.SysFont('arial', 40)
+        # Mostrar el marcador en la parte superior de la pantalla
+        score_text = font.render(f'{puntaje_jugador_1} - {puntaje_jugador_2}', True, (255, 255, 255))
+        self.ventana.blit(score_text, (VENTANA_HORI / 2 - score_text.get_width() / 2, 20))
+        pygame.display.update()
+
 class RaquetaPong:
     def __init__(self, x):
-        ##se definen las caracteristicas de la raqueta
         self.ancho = 20
         self.alto = 100
-
-        ##se definen las coordenadas de la raqueta
         self.x = x
-        self.y = VENTANA_VERT / 2 - self.alto / 2 ##se ubica en el centro de la ventana
+        self.y = VENTANA_VERT / 2 - self.alto / 2
         self.dir_y = 0
         self.raqueta = 0
 
     def mover(self):
-        self.y += self.dir_y ##el self.dir_y es el valor que se le asigna en el evento de teclado
-        if self.y <= 0: ##evita salirse de la ventana hacia arriba, si self.y es menor a 0, se le asigna 0, es decir, se queda en el borde superior
+        self.y += self.dir_y
+        if self.y <= 0:
             self.y = 0
-        if self.y + self.alto >= VENTANA_VERT: ##evita salirse de la ventana hacia abajo, si self.y + self.alto es mayor a VENTANA_VERT, se le asigna VENTANA_VERT - self.alto, es decir, se queda en el borde inferior
+        if self.y + self.alto >= VENTANA_VERT:
             self.y = VENTANA_VERT - self.alto
 
     def dibujar(self, superficie):  
@@ -43,47 +75,39 @@ class PelotaPong:
         self.dir_y = random.choice([-5, 5])
         self.pelota = 0
 
-    def mover(self): ##lanzamiento predeterminado de la pelota
+    def mover(self):
         self.x += self.dir_x
         self.y += self.dir_y
 
     def rebotar(self, raqueta_1, raqueta_2):
-        if self.pelota.colliderect(raqueta_1.raqueta or raqueta_2.raqueta):
-            # Calcular el centro de la raqueta y la pelota
+        if self.pelota.colliderect(raqueta_1.raqueta):
             raqueta_centro = raqueta_1.y + raqueta_1.alto / 2
             pelota_centro = self.y
-            
-            # Cambiar la dirección de la pelota dependiendo de donde golpea la raqueta
             if pelota_centro < raqueta_centro:
-                self.dir_y = -abs(self.dir_y)  # Rebote hacia arriba
+                self.dir_y = -abs(self.dir_y) 
             else:
-                self.dir_y = abs(self.dir_y)   # Rebote hacia abajo
-            
-            self.dir_x = -self.dir_x  # Cambiar la dirección horizontal
+                self.dir_y = abs(self.dir_y)
+            self.dir_x = -self.dir_x 
 
-        # Verificar colisiones con la raqueta 2
         if self.pelota.colliderect(raqueta_2.raqueta):
             raqueta_centro = raqueta_2.y + raqueta_2.alto / 2
             pelota_centro = self.y
-            
             if pelota_centro < raqueta_centro:
-                self.dir_y = -abs(self.dir_y)  # Rebote hacia arriba
+                self.dir_y = -abs(self.dir_y)
             else:
-                self.dir_y = abs(self.dir_y)   # Rebote hacia abajo
-            
-            self.dir_x = -self.dir_x  # Cambiar la dirección horizontal
+                self.dir_y = abs(self.dir_y)
+            self.dir_x = -self.dir_x
 
-        # Rebote en los bordes superior e inferior
         if self.y <= 0 or self.y + self.radio >= VENTANA_VERT:
             self.dir_y = -self.dir_y
 
     def anotacion(self):
         if self.x <= 0:
             self.reiniciar()
-            return 1
+            return 2
         if self.x >= VENTANA_HORI:
             self.reiniciar()
-            return 2
+            return 1
         return 0
 
     def reiniciar(self):
@@ -96,18 +120,31 @@ class PelotaPong:
         self.pelota = pygame.Rect(self.x - self.radio, self.y - self.radio, self.radio * 2, self.radio * 2)
         pygame.draw.circle(superficie, RED, (int(self.x), int(self.y)), self.radio)
 
+class Puntaje:
+    def __init__(self):
+        self.puntaje_jugador_1 = 0
+        self.puntaje_jugador_2 = 0
+
+    def actualizar(self, jugador):
+        if jugador == 1:
+            self.puntaje_jugador_1 += 1
+        elif jugador == 2:
+            self.puntaje_jugador_2 += 1
+
+    def mostrar_en_consola(self):
+        print(f"Puntaje Jugador 1: {self.puntaje_jugador_1} | Puntaje Jugador 2: {self.puntaje_jugador_2}")
+
 def main():
     pygame.init()
-    ventana = pygame.display.set_mode((VENTANA_HORI, VENTANA_VERT))
-    pygame.display.set_caption("Pong Game")
+    ventana = Screen()
 
     pelota = PelotaPong()
     raqueta_1 = RaquetaPong(60)
     raqueta_2 = RaquetaPong(VENTANA_HORI - 60 - 20)
+    puntaje = Puntaje()
 
     jugando = True
     while jugando:
-        ##definioms los eventos de teclado
         for event in pygame.event.get():
             if event.type == QUIT:
                 jugando = False
@@ -119,24 +156,46 @@ def main():
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_w or event.key == pygame.K_s:
                     raqueta_1.dir_y = 0
+        
+        if ventana.game_state == "start_menu":
+            ventana.draw_start_menu()
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_SPACE]:
+                ventana.game_state = "game"
+                
+        elif ventana.game_state == "game_over":
+            ventana.draw_game_over_screen()
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_r]:
+                ventana.game_state = "start_menu"
+            if keys[pygame.K_q]:
+                pygame.quit()
+                quit()
 
-       
+        elif ventana.game_state == "game":
+            # Limpia la pantalla antes de dibujar
+            ventana.ventana.fill(NEGRO)
 
+            pelota.dibujar(ventana.ventana)
+            pelota.mover()
 
-        ventana.fill(NEGRO)
-        pelota.dibujar(ventana)
-        pelota.mover()
+            raqueta_1.dibujar(ventana.ventana)
+            raqueta_1.mover()
 
-        raqueta_1.dibujar(ventana)
-        raqueta_1.mover()
+            raqueta_2.dibujar(ventana.ventana)
+            pelota.rebotar(raqueta_1, raqueta_2)
 
-        raqueta_2.dibujar(ventana)
-        pelota.rebotar(raqueta_1, raqueta_2)
-        pelota.anotacion()  
-        pygame.display.flip()
-        pygame.time.Clock().tick(FPS)
+            anotacion = pelota.anotacion()
+            if anotacion > 0:
+                puntaje.actualizar(anotacion)
+                puntaje.mostrar_en_consola()
 
-    pygame.quit()
+            ventana.draw_score(puntaje.puntaje_jugador_1, puntaje.puntaje_jugador_2)
+            
+            pygame.display.flip()
+            pygame.time.Clock().tick(FPS)
+        
+ 
 
 if __name__ == "__main__":
     main()
